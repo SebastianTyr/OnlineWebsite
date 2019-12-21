a5ffefc475346875f3d710956d49d2dcb287612c
SebastianTyr <48628456+SebastianTyr@users.noreply.github.com>
2019-12-21 23:21:59 +0100

    create local node.js server

---
+++ MyWebsite/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const port = 8080
+
+const app = express()
+
+app.use(express.static(__dirname + '/'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/main_page.html')
+})
+
+app.listen(port, () =>{
+    console.log(`Server runnig at port ${port}`)
+})
