a1a8a5d4bedb81786e56fc6b4b759a0a96499b31
SebastianTyr <48628456+SebastianTyr@users.noreply.github.com>
2020-02-08 00:02:45 +0100

    Project Initial Commit - React new project

--- MyWebsite/main.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import MainPage from "./main_page"
-import "./main.css"
-
-const rootElement = document.getElementById("root")
-ReactDOM.render(<MainPage/>, rootElement)
